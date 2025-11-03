1d6aecc1843742db798b3098554cca71beabfe19
Soulfra <211064529+Soulfra@users.noreply.github.com>
2025-11-03 06:55:36 -0500

    Deploy launcher PWA

---
+++ launcher/sw.js
@@ -0,0 +1,23 @@
+
+// Launcher PWA - Service Worker
+const CACHE_NAME = 'launcher-pwa-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
